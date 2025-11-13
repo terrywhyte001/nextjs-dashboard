2684fd8f61d77decb87aeed25689547a8efe4e49
ievbosaru <ievbosaru@byupathway.edu>
2025-11-13 15:05:35 -0800

    Chapter 15: Add metadata for SEO and shareability

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,14 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
+import { Metadata } from 'next';
 
-export const metadata = {
-  title: 'Acme Dashboard',
-  description: 'Next.js Learn Dashboard',
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Acme Dashboard',
+    default: 'Acme Dashboard',
+  },
+  description: 'The official Next.js Learn Dashboard built with App Router.',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
 };
 
 export default function RootLayout({
