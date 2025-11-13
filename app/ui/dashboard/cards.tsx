d22c65549444b8d8f25ff22e32785b4db66cacb4
ievbosaru <ievbosaru@byupathway.edu>
2025-11-13 13:54:06 -0800

    Chapter 7: Implement data fetching for dashboard with server components

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -17,15 +17,6 @@ export default async function CardWrapper() {
   return (
     <>
       {/* NOTE: Uncomment this code in Chapter 9 */}
-
-      {/* <Card title="Collected" value={totalPaidInvoices} type="collected" />
-      <Card title="Pending" value={totalPendingInvoices} type="pending" />
-      <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
-      <Card
-        title="Total Customers"
-        value={numberOfCustomers}
-        type="customers"
-      /> */}
     </>
   );
 }
@@ -48,8 +39,7 @@ export function Card({
         <h3 className="ml-2 text-sm font-medium">{title}</h3>
       </div>
       <p
-        className={`${lusitana.className}
-          truncate rounded-xl bg-white px-4 py-8 text-center text-2xl`}
+        className={`${lusitana.className} truncate rounded-xl bg-white px-4 py-8 text-2xl`}
       >
         {value}
       </p>
